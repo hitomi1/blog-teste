5360a675f69ecd10a81b0a2613885ae2d6b2959e
Gustavo H <gustavohitomi@gmail.com>
2024-11-06 03:14:26 -0300

    backup

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'home' },
+  { href: '/blog', title: 'blog' },
+  { href: '/projects', title: 'projects' },
+  { href: '/about', title: 'about' },
+  { href: '/resume', title: 'resume' },
+]
+
+export default headerNavLinks
